10a8ade8a668492491b5d446fae5f55c5e24fc3d
Luka Skoric <luka.skoric@dreams-ai.com>
2020-05-21 16:13:56 +0800

    Changed changing shell

--- scripts/set_up_zsh.sh
+++ scripts/set_up_zsh.sh
@@ -1,5 +1,6 @@
 # change the default shell
-sudo chsh -s /usr/bin/zsh root
+sudo apt install -y zsh
+chsh --shell /bin/zsh luka
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
