bc5f362f739bd8cd9314c1de162bf7a409123b47
Skoricius <lukaskoric32@gmail.com>
2023-02-24 15:24:28 +0000

    adding nvim lua setup

--- scripts/set_up_neovim.sh
+++ scripts/set_up_neovim.sh
@@ -1,3 +1,6 @@
 sudo apt install -y neovim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+git clone https://github.com/nvim-lua/kickstart.nvim
+mkdir -p ~/.config/nvim 
+cp -r kickstart.nvim/* ~/.config/nvim/
 
