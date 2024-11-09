50ba244fbf7139ef5c8d05ba54667547132fd670
Lana <lana@Lanas-MacBook-Air.local>
2024-11-09 22:27:37 +0100

    mac setup

--- scripts/set_up_zsh.sh
+++ scripts/set_up_zsh.sh
@@ -3,4 +3,4 @@ sudo apt install -y zsh
 sudo chsh --shell /bin/zsh $USER
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-mv ~/linux_setup/.zshrc ~/.zshrc
+cp ~/linux_setup/.zshrc ~/.zshrc
