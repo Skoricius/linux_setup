0a78247e4b953d6edcc45a7a2a3137e05b71ff4d
Luka Skoric <luka.skoric@riverlane.com>
2025-06-17 23:45:49 +0100

    updating nnn script

--- scripts/nnn.sh
+++ scripts/nnn.sh
@@ -2,3 +2,9 @@ git clone git@github.com:jarun/nnn.git
 cd nnn
 make O_NERD=1
 cp ./nnn ~/.local/bin/nnn
+cd ..
+rm -rf nnn
+mkdir -p ~/.config/nnn/plugins
+cd ~/.config/nnn/plugins
+sh -c "$(curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs)"
+
